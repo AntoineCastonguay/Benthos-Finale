4d60e69b906a2037d6b7734f1630da8bb5f802be
Juliette Robin <jrobin476@gmail.com>
2024-04-22 21:54:19 -0400

    Crétion des 3 figures du rapport

--- R/req_SQL.R
+++ R/req_SQL.R
@@ -3,90 +3,58 @@ SQL.get <- function() {
   con <- dbConnect(SQLite(), dbname="benthos.db")
   
   req <- 
-"SELECT HBI_cat, count(HBI_cat)
-FROM(
-	SELECT 
-		a.site_ab, 
-		a.date_ab, 
-		SUM(CAST(a.abondance AS FLOAT) * e.score_EPT / CAST(abTotal AS FLOAT)) AS HBI,
-		CASE 
-			WHEN SUM(CAST(a.abondance AS FLOAT) * e.score_EPT / CAST(abTotal AS FLOAT)) >= 0 AND SUM(CAST(a.abondance AS FLOAT) * e.score_EPT / CAST(abTotal AS FLOAT)) < 3.51 THEN 'Excellente'
-			WHEN SUM(CAST(a.abondance AS FLOAT) * e.score_EPT / CAST(abTotal AS FLOAT)) >= 3.51 AND SUM(CAST(a.abondance AS FLOAT) * e.score_EPT / CAST(abTotal AS FLOAT)) < 4.51 THEN 'Très bonne'
-			WHEN SUM(CAST(a.abondance AS FLOAT) * e.score_EPT / CAST(abTotal AS FLOAT)) >= 4.51 AND SUM(CAST(a.abondance AS FLOAT) * e.score_EPT / CAST(abTotal AS FLOAT)) < 5.51 THEN 'Bonne'
-			WHEN SUM(CAST(a.abondance AS FLOAT) * e.score_EPT / CAST(abTotal AS FLOAT)) >= 5.51 AND SUM(CAST(a.abondance AS FLOAT) * e.score_EPT / CAST(abTotal AS FLOAT)) < 6.51 THEN 'Moyenne'
-			WHEN SUM(CAST(a.abondance AS FLOAT) * e.score_EPT / CAST(abTotal AS FLOAT)) >= 6.51 AND SUM(CAST(a.abondance AS FLOAT) * e.score_EPT / CAST(abTotal AS FLOAT)) < 7.51 THEN 'Plutôt mauvaise'
-			WHEN SUM(CAST(a.abondance AS FLOAT) * e.score_EPT / CAST(abTotal AS FLOAT)) >= 7.51 AND SUM(CAST(a.abondance AS FLOAT) * e.score_EPT / CAST(abTotal AS FLOAT)) < 8.51 THEN 'Mauvaise'
-			WHEN SUM(CAST(a.abondance AS FLOAT) * e.score_EPT / CAST(abTotal AS FLOAT)) >= 8.51 AND SUM(CAST(a.abondance AS FLOAT) * e.score_EPT / CAST(abTotal AS FLOAT)) < 10.01 THEN 'Très mauvaise'
-			ELSE 'Autre Interval'
-		END AS HBI_cat
-	FROM 
-		espece e
-		INNER JOIN abondance a ON a.identification_ab = e.identification
-		INNER JOIN site s ON s.site = a.site_ab AND s.date = a.date_ab
-		INNER JOIN (
+  "WITH abondances_par_echantillonnage AS (
 			SELECT 
-				site_ab,
-				date_ab,
-				SUM(abondance) AS abTotal
-			FROM 
-				abondance
-			GROUP BY 
-				site_ab,
-				date_ab
-		) AS abT ON (a.site_ab = abT.site_ab AND a.date_ab = abT.date_ab)
-	GROUP BY 
-		a.site_ab, 
-		a.date_ab
-	ORDER BY 
-		a.site_ab)
-GROUP BY
-	HBI_cat
-ORDER BY
-	CASE 
-		WHEN HBI_cat = 'Excellente' THEN 1
-		WHEN HBI_cat = 'Très bonne' THEN 2
-		WHEN HBI_cat = 'Bonne' THEN 3
-		WHEN HBI_cat = 'Moyenne' THEN 4
-		WHEN HBI_cat = 'Plutôt mauvaise' THEN 5
-		WHEN HBI_cat = 'Mauvaise' THEN 6
-		WHEN HBI_cat = 'Très mauvaise' THEN 7
-	END"
+				date_ab, 
+				site_ab, 
+				SUM(abondance) / fraction AS  abondance_site
+			FROM abondance
+			INNER JOIN site ON (site = site_ab AND date = date_ab)
+			GROUP BY date_ab, site_ab
+			),
+			indice_pollution_par_echantillonnage AS (
+			SELECT date_ab, site_ab,
+				SUM((abondance / fraction) * score_EPT / abondance_site) AS indice_pollution
+			FROM abondance 
+			INNER JOIN espece ON identification_ab = identification
+			INNER JOIN abondances_par_echantillonnage USING (site_ab, date_ab)
+			INNER JOIN site ON (site = site_ab AND date = date_ab)
+			GROUP BY date_ab, site_ab
+			),
+			categories_pollution AS (
+			SELECT 'Excellente' AS categorie, 0 AS minimum, 3.50 AS maximum, 1 AS ordre
+			UNION ALL
+			SELECT 'Très bonne', 3.50, 4.5, 2
+			UNION ALL
+			SELECT 'Bonne', 4.5, 5.5, 3
+			UNION ALL
+			SELECT 'Moyenne', 5.5, 6.5, 4
+			UNION ALL
+			SELECT 'Plutôt mauvaise', 6.5, 7.5, 5
+			UNION ALL
+			SELECT 'Mauvaise', 7.5, 8.5, 6
+			UNION ALL
+			SELECT 'Très mauvaise', 8.5, 9.5, 7
+			)
+			
+	SELECT 
+		categories_pollution.categorie categorie_pollution, 
+		COUNT(site_ab) nombre_rivieres,
+		ordre
+	FROM categories_pollution
+	LEFT JOIN (
+    SELECT 
+    	site_ab, 
+    	AVG(indice_pollution) as indice_pollution
+    FROM indice_pollution_par_echantillonnage
+    GROUP BY site_ab
+	) rivieres ON (rivieres.indice_pollution <= categories_pollution.maximum AND rivieres.indice_pollution > categories_pollution.minimum)
+	GROUP BY categorie, ordre
+	ORDER BY ordre DESC"
   
   res <- dbGetQuery(con, req)
   
   req2 <- 
-"SELECT site_ab, AVG(parDate.abTotal) AS abTotal, AVG(sc) AS HBI
-FROM
-	(SELECT 
-		a.site_ab, 
-		a.date_ab, 
-		abTotal,
-		SUM(CAST(a.abondance AS FLOAT)* e.score_EPT / CAST(abTotal AS FLOAT)) AS sc
-	FROM 
-		espece e
-		INNER JOIN abondance a ON a.identification_ab = e.identification
-		INNER JOIN site s ON s.site = a.site_ab AND s.date = a.date_ab
-		INNER JOIN (
-			SELECT 
-				site_ab,
-				date_ab,
-				SUM(abondance) AS abTotal
-			FROM 
-				abondance
-			GROUP BY 
-				site_ab,
-				date_ab
-		) AS abT ON (a.site_ab = abT.site_ab AND a.date_ab = abT.date_ab)
-	GROUP BY 
-		a.site_ab, 
-		a.date_ab
-	ORDER BY a.site_ab) AS parDate
-GROUP BY site_ab
-ORDER BY site_ab"
-  
-  res2 <- dbGetQuery(con, req2)
-  
-  req3 <- 
 "SELECT site_ab, AVG(parDate.richesse) AS richesse, AVG(sc) AS HBI
 FROM
 	(SELECT 
@@ -127,9 +95,34 @@ FROM
 GROUP BY site_ab
 ORDER BY site_ab"
   
+  res2 <- dbGetQuery(con, req2)
+  
+  req3 <- 
+    "WITH 
+			richesse_par_echantillonnage AS (
+			SELECT 
+				date_ab, 
+				site_ab, 
+				COUNT(abondance) AS  richesse_echantillonnage
+			FROM abondance
+			INNER JOIN site ON (site = site_ab AND date = date_ab)
+			GROUP BY date_ab, site_ab
+			)
+			
+    SELECT 
+    	site_ab, 
+    	AVG(richesse_echantillonnage) as richesse,
+    	AVG(temperature_eau_c) as temperature_eau_c
+    FROM richesse_par_echantillonnage
+    INNER JOIN condition_echantillonnage ON (date_ab = date_cond AND site_ab = site_cond)
+    GROUP BY site_ab
+    ORDER BY site_ab"
+  
   res3 <- dbGetQuery(con, req3)
   
   dbDisconnect(con)
   
-  return(list(res,res2,res3))
+  results <- list(res,res2,res3)
+  
+  return(results)
 }
